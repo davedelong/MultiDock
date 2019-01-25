0c4c6487d51ad2cb4870a79073ced53cf5fd07b1
Dave DeLong <davedelong@me.com>
2019-01-25 16:31:55 -0700

    Initial Commit

---
+++ MultiDock/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MultiDock
+//
+//  Created by Dave DeLong on 1/25/19.
+//  Copyright © 2019 Syzygy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
